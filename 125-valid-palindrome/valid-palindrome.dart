cb1be08410bafbee01cd1a13f8340dc303f7994c
wafaa boukli <86812397+Boukli-h@users.noreply.github.com>
2024-01-14 03:18:14 +0100

    Time: 370 ms (35.64%) | Memory: 149.7 MB (47.34%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.dart
+++ 125-valid-palindrome/valid-palindrome.dart
@@ -1,7 +1,7 @@
 class Solution {
   bool isPalindrome(String s) {
   // Convert to lowercase and remove non-alphanumeric characters
-  String cleanedString = s.toLowerCase().replaceAll(RegExp(r'[^a-z0-9]'), '');
+  String cleanedString = s.toLowerCase().replaceAll(RegExp('[^a-z0-9]'), '');
 
   // Check if the cleaned string is a palindrome
   return cleanedString == cleanedString.split('').reversed.join('');
